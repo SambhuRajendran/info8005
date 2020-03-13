f5269596af148af80cb9ebd0a699bcdf6fef79e4
sambhu619 <rsambhu619@gmail.com>
2020-03-13 19:42:06 -0300

    Added new code with pythonfileread

---
+++ mathoperator_readfiles.py
@@ -0,0 +1,23 @@
+def math(value1, value2):
+    print(value1, value2)
+    print('Addition: ', value1+value2)
+    print('Subtraction: ', value1-value2)
+    print('Multiplication: ', value1*value2)
+    print('Division: ', value1/value2)
+    print('Modulus: ', value1%value2)
+    print('Exponent: ', value1**value2)
+    print('FloorDivision:', value1//value2)
+
+filePath= 'S:\\INFO8005_PY\\'
+firstfiletoread= 'CondorID_Value1.txt'
+secondfiletoread= 'CondorID_Value2.txt'
+#print(firstfiletoread)
+readfirstfile = open(filePath+ firstfiletoread, "r")
+readsecondfile = open(filePath+ secondfiletoread, "r")
+firstfiledata= int(readfirstfile.read())
+print(type(firstfiledata))
+secondfiledata= int(readsecondfile.read())
+print(type(secondfiledata))
+
+ 
+math(firstfiledata, secondfiledata)
