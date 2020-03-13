3b082bec2f2444bdeddbe87751e0318dc381563f
sambhu619 <rsambhu619@gmail.com>
2020-03-13 19:05:43 -0300

    py file

---
+++ mathoperator.py
@@ -0,0 +1,13 @@
+def math(value1, value2):
+    print('Addition: ', value1+value2)
+    print('Subtraction: ', value1-value2)
+    print('Multiplication: ', value1*value2)
+    print('Division: ', value1/value2)
+    print('Modulus: ', value1%value2)
+    print('Exponent: ', value1**value2)
+    print('FloorDivision:', value1//value2)
+
+CondorID_Value1 = 42
+CondorID_Value2 = 18
+
+math(CondorID_Value1, CondorID_Value2)
